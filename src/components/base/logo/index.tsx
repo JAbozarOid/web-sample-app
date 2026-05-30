1909817490e956b381b7314b54b09e74308f41fd
jabozaroid <araghibdoust@hotmail.com>
2026-05-31 00:14:53 +0330

    logo_added

---
+++ src/components/base/logo/index.tsx
@@ -0,0 +1,24 @@
+import React, { type JSX } from "react";
+
+type LogoProps = {
+  size?: "small" | "medium" | "large";
+};
+
+const sizes = {
+  small: { width: 60, height: 60 },
+  medium: { width: 120, height: 120 },
+  large: { width: 180, height: 180 },
+};
+
+const Logo: React.FC<LogoProps> = ({ size = "medium" }): JSX.Element => {
+  return (
+    <img
+      src="/assets/images/logo.png"
+      width={sizes[size].width}
+      height={sizes[size].height}
+      alt=""
+    />
+  );
+};
+
+export default Logo;
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,38 +1,28 @@
 import { ButtonVersion2 } from "src/components/base/button";
+import Logo from "../base/logo";
+import Container from "../base/container";
 
 export const Header: React.FC = () => {
   return (
     <header>
-      <ButtonVersion2
-        variant="contained"
-        color="primary"
-        size="small"
-        onClick={() => {
-          console.log("Button Clicked");
-        }}
-      >
-        contained
-      </ButtonVersion2>
-      <ButtonVersion2
-        variant="outlined"
-        color="primary"
-        size="small"
-        onClick={() => {
-          console.log("Button Clicked");
-        }}
-      >
-        contained
-      </ButtonVersion2>
-      <ButtonVersion2
-        variant="text"
-        color="primary"
-        size="small"
-        onClick={() => {
-          console.log("Button Clicked");
-        }}
-      >
-        contained
-      </ButtonVersion2>
+      <Container>
+        <div className="flex justify-between items-center">
+          <div>
+            <Logo />
+          </div>
+          <div>
+            <ButtonVersion2 variant="text" color="primary">
+              کلیک کن
+            </ButtonVersion2>
+            <ButtonVersion2 variant="text" color="primary">
+              کلیک کن
+            </ButtonVersion2>
+            <ButtonVersion2 variant="contained" color="primary">
+              کلیک کن
+            </ButtonVersion2>
+          </div>
+        </div>
+      </Container>
     </header>
   );
 };
